2a853f2c1ab15138ed0d526918cfa7e5066f1edd
EileenSanchez1 <eileensanchez49@gmail.com>
2026-08-20 14:19:39 -0500

    endpoint aprendices funcionando

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
-import {configDotenv} from "dotenv"
-configDotenv()
+require('dotenv').config();
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
+
+//libreria para leer archivos
+const sistemaArchivos = require('fs'); 
+const ruta = require('path');
+
+//generar una ruta para listaDatos.json
+const rutaArchivojson = ruta.join(__dirname, 'listaDatos.json');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Servidor inicializado correctamente');
+});
+
+//endpoint para obtener todos los aprendices
+app.get('/api/aprendices', (req, res)=> {
+    sistemaArchivos.readFile(rutaArchivojson, "utf-8", (error, data)=> { 
+        if (error) {
+            return res.status(500).json({Error: "Error al leer el archivo , conexion a la bd"}); 
+        }
+        const listaAprendices = JSON.parse(data); 
+        res.json(listaAprendices);
+    });
 });
 
 app.listen(PORT, () => {
